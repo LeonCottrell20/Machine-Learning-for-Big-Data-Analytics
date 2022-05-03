4120f496d019d62b38dc8158d2606ae7a91358cc
LeonCottrell20 <73644572+LeonCottrell20@users.noreply.github.com>
2022-05-03 21:20:54 +0100

    Update mpg_auto_regression.py

--- mpg_auto_regression.py
+++ mpg_auto_regression.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1rpiNzO8mzBUMrlqcDmq2esBlitcpTcxp
 """
 
-!pip install pyod
+
 import os
 import numpy as np
 import pandas as pd
